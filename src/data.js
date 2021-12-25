25e5a50046354bd5b82af0d168c3e243b85fc1e2
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2021-12-25 16:50:52 +0530

    cleared

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data={
+    productData:[
+        {
+            id:1,
+            img:"https://5.imimg.com/data5/SELLER/Default/2020/10/VG/SA/VY/19498524/rose-gold-american-diamong-chand-bali-earrings-500x500.jpeg",
+            title:"Stone Work",
+            desc:"",
+            price:450
+        },
+        {
+            id:2,
+            img:"https://n1.sdlcdn.com/imgs/i/p/l/Oxidized-Party-Wear-Jhumka-Earring-SDL869986982-1-0f237.jpeg",
+            title:"Oxidized",
+            desc:"",
+            price:700
+        },
+        {
+            id:3,
+            img:"https://s3-us-east-2.amazonaws.com/ethnicpipproduction/avatars/original/IMG-20191108-WA0000-aa3bd54fb6189b1627528c2c8e476988d21097779ce7abdc1bcc42d8d0824f74.jpg",
+            title:"Teracotta",
+            desc:"",
+            price:550
+        }
+    ],
+
+};
+export default data;
